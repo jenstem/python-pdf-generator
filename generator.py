34ab558a5c68c2b91053cd9ae291d2ed850bd65e
Jen <jennifer.stemkowski@gmail.com>
2024-04-16 15:03:15 -0400

    Align + add border to header + add padding

--- generator.py
+++ generator.py
@@ -4,12 +4,13 @@ from fpdf import FPDF
 class PDF(FPDF):
     def header(self):
         self.image("logo.jpg", 10, 8, 33)
+        self.set_font("Arial", "B", 16)
+        self.cell(80)
+        self.cell(40, 10, "Hello World!", border = 1, align = "C")
 
 
 pdf = PDF()
 
 
 pdf.add_page()
-pdf.set_font("Arial", "B", 16)
-pdf.cell(40, 10, "Hello World!")
 pdf.output("sample.pdf")
