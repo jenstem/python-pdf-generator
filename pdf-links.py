b88c245ad11d1ed0017a6166a9ff79fb326af241
Jen <jennifer.stemkowski@gmail.com>
2024-04-16 18:02:46 -0400

    Add html code to pdf file

--- pdf-links.py
+++ pdf-links.py
@@ -12,6 +12,12 @@ pdf.write(5, "here", link)
 
 pdf.add_page()
 pdf.image("logo.jpg", 10, 10, 50, 0, "", "https://www.google.com")
-
+pdf.set_left_margin(60)
+pdf.set_font_size(18)
+pdf.write_html("<h1>Heading</h1>")
+pdf.write_html(""" <b>Sub-Title</b>
+                <br>
+                <a href="https://www.google.com">Link to Google</a>
+                """)
 
 pdf.output("link.pdf")
