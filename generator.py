121897d91ee858873bd3d33c9fc8c2737e5953e0
Jen <jennifer.stemkowski@gmail.com>
2024-04-16 15:53:29 -0400

    Add chapter_title func

--- generator.py
+++ generator.py
@@ -8,8 +8,10 @@ class PDF(FPDF):
     def footer(self):
         pass
 
-    def chapter_title(self):
-        pass
+    def chapter_title(self, num, label):
+        self.set_font("Arial", "B", 16)
+        self.set_fill_color(200, 220, 255)
+        self.cell(0, 6, f"Chapter {num} : {label}", new_x = "LMARGIN", new_y = "NEXT", align = "L", fill = True)
 
     def chapter_body(self, filepath):
         with open(filepath, "rb") as fh:
@@ -20,12 +22,12 @@ class PDF(FPDF):
         self.set_font(style = "I")
         self.cell(0, 5, "(End of excerpt)")
 
-    def print_chapter(self, filepath):
+    def print_chapter(self, num, title, filepath):
         self.add_page()
+        self.chapter_title(num, title)
         self.chapter_body(filepath)
-        pass
 
 
 pdf = PDF()
-pdf.print_chapter("para.txt")
+pdf.print_chapter(1, "Getting Started with Programming", "para.txt")
 pdf.output("sample2.pdf")
